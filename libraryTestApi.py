5640bd46315e9e459d474f8df40f05c1aa5c424a
Ronak Gavandi <ronakg11@gmail.com>
2023-03-13 23:38:02 -0700

    commented print(response.text) as it wasn't needed

--- libraryTestApi.py
+++ libraryTestApi.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.get('http://216.10.245.166/Library/GetBook.php', params={'ID': 'babshi001'},)
-print(response.text)
+# print(response.text)
 list_resp = response.json()
 print(list_resp[0]['author'])
 assert response.status_code == 200
